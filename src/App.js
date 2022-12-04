0db09e1b86eea9673f7c467f430269267d1c2bdf
Your Name <you@example.com>
2022-12-04 20:17:19 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import styled from 'styled-components';
+import { ColorSuccess } from './js/variables'
+
+const Heading = styled.h1`
+  color: ${ColorSuccess};
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> hello, this is header1</h1>
+        <Heading>hello, i am success</Heading>
         <h2>this is header2</h2>
         <h2> </h2>
       </header>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import 'build/css/_variables.css';
 import './index.css';
 import App from './App';
 
